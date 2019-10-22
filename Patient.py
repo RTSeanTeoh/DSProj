a36b6d6be3f9c496b2c9a6fdaff68f47820d3afc
RTSeanTeoh <seanteohrt@gmail.com>
2019-10-22 22:54:58 +0800

    New

--- Patient.py
+++ Patient.py
@@ -1,3 +1,8 @@
+import time;
+
 class Patient:
-    def __init__(self, critlevel, patientName):
-        self.critlevel = critlevel
+    def __init__(self, crit_level, patient_name, patient_number):
+        self.crit_level = crit_level
+        self.patient_name = patient_name
+        self.patient_number = patient_number
+        patient_entry_time = time.time()
