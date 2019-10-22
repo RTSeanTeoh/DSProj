6f421c5f888742819366d335ae16b9a42f00ca4a
RTSeanTeoh <seanteohrt@gmail.com>
2019-10-22 22:17:58 +0800

    Test

---
+++ EmergencyRoomQueue.py
@@ -0,0 +1,31 @@
+
+
+def main():
+    print("Please do some shit")
+
+#Function that will retrieve patient information and sort it into the linked list
+def insertPatientIntoList():
+
+
+#Function that allows patient records to be accessed and manipulated in the list
+def accessPatientRecord():
+
+
+# ---------------------------------------#
+# Function that forces the values in the #
+# list to sort                           #
+# Usage is to allow a mid section update #
+# in a patient file                      #
+# ---------------------------------------#
+
+def queueListSort():
+
+
+#---------------------------------------#
+# Function that requests a patient file #
+# from the queue for the doctor then    #
+# saves and closes the file             #
+#---------------------------------------#
+def closePatientFile():
+
+main()
---
+++ Patient.py
@@ -0,0 +1,3 @@
+class Patient:
+    def __init__(self, critlevel, patientName):
+        self.critlevel = critlevel
